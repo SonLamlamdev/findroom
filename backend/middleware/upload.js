8edc7d79e4079c65c4e87de5e8de78f9fad14243
MoreLikeBean <61461251+snoob122@users.noreply.github.com>
2025-12-13 21:27:19 +0700

    added peer and attempt fix CS

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -13,8 +13,9 @@ if (useCloudinary) {
   // Try to use Cloudinary
   try {
     const cloudinary = require('cloudinary').v2;
-    const { CloudinaryStorage } = require('multer-storage-cloudinary');
-
+    //const { CloudinaryStorage } = require('multer-storage-cloudinary');
+    const CloudinaryStorage = require('multer-storage-cloudinary');
+    
     cloudinary.config({
       cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
       api_key: process.env.CLOUDINARY_API_KEY,
