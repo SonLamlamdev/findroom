d06d3793c424767c74d59477e8c429f12e8eb2f2
MoreLikeBean <61461251+snoob122@users.noreply.github.com>
2025-12-14 17:46:02 +0700

    detach from LD

--- frontend/src/i18n/config.ts
+++ frontend/src/i18n/config.ts
@@ -1,6 +1,5 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 
 const resources = {
   vi: {
@@ -526,7 +525,6 @@ const resources = {
 };
 
 i18n
-  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources,
